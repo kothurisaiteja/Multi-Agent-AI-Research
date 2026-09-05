0eb20234aa6e5af50ae09ab42752ff21fe9e1c93
Sai Teja <YOUR_GITHUB_NOREPLY_EMAIL>
2026-09-05 20:12:30 +0530

    commit

--- Backend/main.py
+++ Backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://multi-agent-ai-research-byteja.vercel.app/"],
+    allow_origins=["https://multi-agent-ai-research-byteja.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
